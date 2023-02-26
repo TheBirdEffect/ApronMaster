88f61f808bdd815527e2576a5ef2c6fec4ef1fae
DrArtfulBird <56632916+TheBirdEffect@users.noreply.github.com>
2023-02-26 22:20:40 +0100

    Added cors options

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  constructor(private http:HttpClient) {}
+  flights: any;
+
+  title = "Client"
+  
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/flight").subscribe({
+      next: response => this.flights = response,
+      error: error => console.log(error),
+      complete: () => console.log("Response completed!")
+    })
+  }
+
+
+  
 }
