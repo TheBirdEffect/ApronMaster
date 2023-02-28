bd2daace7649396e19062878459ab551fe0a0de3
DrArtfulBird <56632916+TheBirdEffect@users.noreply.github.com>
2023-02-28 10:28:10 +0100

    ActionController Example added

--- API/Controllers/ActionController.cs
+++ API/Controllers/ActionController.cs
@@ -2,16 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entity;
 using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
 
 namespace API.Controllers
 {
     public class ActionController : BaseController
     {
+        private readonly DataContext _context;
+
+        public ActionController(DataContext context)
+        {
+            _context = context;
+        }
+
+
         [HttpGet("calculate")]
         public void shouldCalculateRoutes()
         {
-            Console.WriteLine("Route was triggered!");
+            var flights = _context.Flights.ToList();
+            Console.WriteLine($"The number of flights is currently {flights.Count()}");
+            foreach(var flight in flights)
+            {
+                Console.WriteLine($"{flight.FlightId} {flight.FlightNo} {flight.Arrival} {flight.Departure} {flight.Destination} {_context.AircraftTypes.Find(flight.AircraftTypeId).Name}");
+            }
         }
     }
 }
