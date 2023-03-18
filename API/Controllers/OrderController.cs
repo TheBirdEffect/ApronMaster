a1fe209a01c773bcb9fadd58cfa5b2b6b60dc322
DrArtfulBird <56632916+TheBirdEffect@users.noreply.github.com>
2023-03-18 13:57:48 +0100

    Order Controller extended with new function AssembleOrder -> HttpGet

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -31,14 +31,70 @@ namespace API.Controllers
             return await _context.Orders.FindAsync(id);
         }
 
-        [HttpGet("full{id}")]
-        public async Task<ActionResult<Order>> AssembleOrder(int id) {
-            var assembledOrder =    from o in _context.Orders
+        [HttpGet("flight{id}")]
+        public async Task<ActionResult<IEnumerable<Order>>> AssembleOrder(int id)
+        {
+            return await (from o in _context.Orders
                                         join p in _context.Positions
-                                            on o.PositionId equals p.PositionId
-                                    select new {
-                                        
-                                    }
-    }    
-        // https://www.c-sharpcorner.com/UploadFile/ff2f08/sql-join-in-linq-linq-to-entity-linq-to-sql/
+                                        on o.PositionId equals p.PositionId into __positions
+                                    from pos in __positions.DefaultIfEmpty()
+
+                                        join f in _context.Flights
+                                        on o.FlightId equals f.FlightId into __flights
+                                    from flight in __flights.DefaultIfEmpty()
+
+                                        join vT in _context.VehicleTypes
+                                        on o.VehicleTypeId equals vT.VehicleTypeId into __vehicleTypes
+                                    from vehicleType in __vehicleTypes.DefaultIfEmpty()
+
+                                 where o.FlightId == id
+                                 select new Order
+                                 {
+                                     OrderId = o.OrderId,
+                                     StartOfService = o.StartOfService,
+                                     EndOfService = o.EndOfService,
+                                     QtyFuel = o.QtyFuel,
+                                     PositionId = o.PositionId,
+                                     Position = new Position
+                                     {
+                                         PositionId = pos.PositionId,
+                                         Name = pos.Name,
+                                         IsGate = pos.IsGate
+                                     },
+                                     FlightId = o.FlightId,
+                                     Flight = new Flight 
+                                     {
+                                        FlightId = flight.FlightId,
+                                        AircraftTypeId = flight.AircraftTypeId,
+                                        Arrival = flight.Arrival,
+                                        Departure = flight.Departure,
+                                        Destination = flight.Destination,
+                                        FlightNumber = flight.FlightNumber
+                                     },
+                                     VehicleTypeId = o.VehicleTypeId,
+                                     VehicleType = new VehicleType {
+                                        VehicleTypeId = vehicleType.VehicleTypeId,
+                                        Name = vehicleType.Name
+                                     }
+                                 }).ToListAsync();
+
+            // Console.WriteLine("OrderId\tSoS\tEoS\tQtyFuel\tPosId\tPosName\tIsGate\tFlightID\tVehTypeID");
+            // foreach (Order data in assembledOrder)
+            // {
+            //     Console.WriteLine(data.OrderId
+            //     + "\t" + data.StartOfService
+            //     + "\t" + data.EndOfService
+            //     + "\t" + data.QtyFuel
+            //     + "\t" + data.PositionId
+            //     + "\t" + data.Position.PositionId
+            //     + "\t" + data.Position.Name
+            //     + "\t" + data.Position.IsGate
+            //     + "\t" + data.FlightId
+            //     + "\t" + data.VehicleTypeId
+            //     );
+            // }
+
+        }
+    }
+    // https://www.c-sharpcorner.com/UploadFile/ff2f08/sql-join-in-linq-linq-to-entity-linq-to-sql/
 }
