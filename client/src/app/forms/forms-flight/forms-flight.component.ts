597e4fa05e0901bd5277dc987fbfdb3c4b928954
DrArtfulBird <56632916+TheBirdEffect@users.noreply.github.com>
2023-03-24 17:04:24 +0100

    Replaced forms with reactiveForms

--- client/src/app/forms/forms-flight/forms-flight.component.ts
+++ client/src/app/forms/forms-flight/forms-flight.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { first } from 'rxjs';
+import { first, map } from 'rxjs';
 import { AircraftType } from '../../_models/aircraftType';
 import { Flight } from '../../_models/flight';
 import { AircraftTypesService } from '../../_service/aircraft-types.service';
@@ -18,17 +18,17 @@ export class FormsFlightComponent implements OnInit {
   model: any = {}
 
   id: number;
-  form!: FormGroup
+  form: FormGroup
   title = "Flight";
   loading = false;
   submitted = false;
 
 
-  constructor(private flightService: FlightsService, 
-              private aircrafttypeService: AircraftTypesService,
-              private formBuilder: FormBuilder,
-              private route: ActivatedRoute
-            ) { }
+  constructor(private flightService: FlightsService,
+    public aircrafttypeService: AircraftTypesService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.getAircraftTypes();
@@ -41,33 +41,21 @@ export class FormsFlightComponent implements OnInit {
       destination: ['', Validators.required],
       aircraftType: ['', Validators.required]
     });
-
-    this.title = 'Add Flight';
-    if (this.id) {
-      //edit mode
-      this.title = 'Edit Flight';
-      this.loading = true;
-      this.flightService.getFlightById(this.id)
-      .pipe(first())
-      .subscribe(flight => {
-        this.form.patchValue(flight);
-        this.loading = false;
-      })    
-    }
   }
 
   //https://jasonwatmore.com/post/2022/12/05/angular-14-dynamic-add-edit-form-that-supports-create-and-update-mode#users-add-edit-component-ts
   // Hier gehts weiter
 
-  addFlight() {
-    this.flightService.addFlight(this.model).subscribe({
+  get f() { return this.form.controls; }
+
+
+
+  addFlight(model: any) {
+    this.flightService.addFlight(model).subscribe({
       next: response => {
-        this.model = response
-      }, 
-      error: error => console.log(error),
-      complete: () =>  {
-        this.cancel()
-      }
+        //this.model = response
+      },
+      error: error => console.log(error)
     });
   }
 
@@ -79,8 +67,13 @@ export class FormsFlightComponent implements OnInit {
     })
   }
 
-  onSubmit() {
-
+  onSubmit(form: FormGroup) {
+    const flight = this.mapFlight(form);    
+    this.addFlight(flight);
+    this.cancel()
+  }
+  mapFlight(form: FormGroup): Flight {
+    return form.value;
   }
 
   cancel() {
