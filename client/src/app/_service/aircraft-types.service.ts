3c297578332ae27506413b8b562752d85683c499
DrArtfulBird <56632916+TheBirdEffect@users.noreply.github.com>
2023-03-24 17:03:55 +0100

    Added ovservable for async loading

--- client/src/app/_service/aircraft-types.service.ts
+++ client/src/app/_service/aircraft-types.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { AircraftType } from '../_models/aircraftType';
 
 @Injectable({
@@ -8,10 +8,17 @@ import { AircraftType } from '../_models/aircraftType';
 })
 export class AircraftTypesService {
   baseUrl = "https://localhost:5001/api";
+  typesSource = new BehaviorSubject<AircraftType[] | null>(null);
+  currentType$ = this.typesSource.asObservable();
 
   constructor(private http:HttpClient) { }
 
   GetAircraftTypes() {
-    return this.http.get<AircraftType>(this.baseUrl + "/AircraftType");
+    return this.http.get<AircraftType[]>(this.baseUrl + "/AircraftType").pipe(
+      map((response: AircraftType[]) => {
+        this.typesSource.next(response);
+        console.log('AircraftTypes', this.typesSource.getValue());
+      })
+    );
   }
 }
