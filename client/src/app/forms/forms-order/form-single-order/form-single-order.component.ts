8db54b179f6ccfa468ee0f3fdde683f3edd95811
TheBirdEffect <christoph.frischmuth@web.de>
2023-04-03 12:26:18 +0200

    Prepared dynamic add of orders

--- client/src/app/forms/forms-order/form-single-order/form-single-order.component.ts
+++ client/src/app/forms/forms-order/form-single-order/form-single-order.component.ts
@@ -1,5 +1,6 @@
+import { formatCurrency } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Flight } from 'src/app/_models/flight';
 import { position } from 'src/app/_models/position';
@@ -13,50 +14,64 @@ import { VehicleService } from 'src/app/_service/vehicle.service';
   templateUrl: './form-single-order.component.html',
   styleUrls: ['./form-single-order.component.scss']
 })
-export class FormSingleOrderComponent implements OnInit{
-@Output() closeModal = new EventEmitter();
+export class FormSingleOrderComponent implements OnInit {
+  @Output() closeModal = new EventEmitter();
 
-singleOrderForm: FormGroup;
-flights$: Observable<Flight[] | null>;
-vehicleTypes$: Observable<vehicleType[] | null>;
-positions$: Observable<position[] | null>;
+  singleOrderForm: FormGroup;
+  formArray: FormArray;
+  flights$: Observable<Flight[] | null>;
+  vehicleTypes$: Observable<vehicleType[] | null>;
+  positions$: Observable<position[] | null>;
 
-constructor(private flightService: FlightsService
-            , private vehicleTypeService: VehicleService
-            , private positionService: PositionService
-            , private formBuilder: FormBuilder
-            ) {}
+  //https://blog.angular-university.io/angular-form-array/
 
-ngOnInit(): void {
-  this.positionService.getPositions().subscribe();
-  this.vehicleTypeService.GetVehicleTypes().subscribe();
+  constructor(private flightService: FlightsService
+    , private vehicleTypeService: VehicleService
+    , private positionService: PositionService
+    , private formBuilder: FormBuilder
+  ) { }
 
-  this.vehicleTypes$ = this.vehicleTypeService.loadVehicles();
-  this.flights$ = this.flightService.loadFlights();
-  this.positions$ = this.positionService.loadPositions()
+  ngOnInit(): void {
+    this.positionService.getPositions().subscribe();
+    this.vehicleTypeService.GetVehicleTypes().subscribe();
 
-  this.singleOrderForm = this.formBuilder.group({
-    flight: ['', Validators.required],
-    vehicleType: ['', Validators.nullValidator],
-    position: ['', Validators.required],
-    startOfService: ['', Validators.required],
-    endOfService: ['', Validators.required],
-    fuel: ['NULL', Validators.nullValidator],
-    fuelAmmount: ['', Validators.nullValidator]
-  })
-}
+    this.vehicleTypes$ = this.vehicleTypeService.loadVehicles();
+    this.flights$ = this.flightService.loadFlights();
+    this.positions$ = this.positionService.loadPositions()
 
-onSubmit(form: FormGroup<any>) {
-  console.log('Order Object', this.singleOrderForm.value);
-  
-}
+    this.singleOrderForm = this.formBuilder.group({
+      orders: this.formBuilder.array([])
+    })
+  }
 
-updateAircraftTypeByFlight() {
+  onSubmit(form: FormGroup<any>) {
+    //console.log('Order Object', this.singleOrderForm.value);
+  }
 
-}
+  get orders() {
+    return this.singleOrderForm.controls['orders'] as FormArray;
+  }
 
-cancel() {
-  this.closeModal.emit(true)
-}
+  newSingleForm() {
+    const orderForm = this.formBuilder.group({
+      flight: ['', Validators.required],
+      vehicleType: ['', Validators.nullValidator],
+      position: ['', Validators.required],
+      startOfService: ['', Validators.required],
+      endOfService: ['', Validators.required],
+      fuel: ['NULL', Validators.nullValidator],
+      fuelAmmount: ['', Validators.nullValidator]
+    })
+
+    this.orders.push(orderForm);
+  }
+
+  updateAircraftTypeByFlight() {
+
+  }
+
+  cancel() {
+    this.closeModal.emit(true)
+  }
 
 }
