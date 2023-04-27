72205f31fc5c2969a17e8384cc7f079b46b3a4ce
DrArtfulBird <56632916+TheBirdEffect@users.noreply.github.com>
2023-04-27 17:57:33 +0200

    added some properties to the base model

--- API/Models/SchedulingBaseModel.cs
+++ API/Models/SchedulingBaseModel.cs
@@ -10,7 +10,10 @@ namespace API.Models
     {
         public Flight Flight { get; set; }
         public GroundVehicle Vehicle { get; set; }
+        public Order Order { get; set; }
+        public TimeSpan timeToRun { get; set; }
         public DateTime eSoS { get; set; }
         public DateTime Deadline { get; set; }
+        public double Slack { get; set; }
     }
 }
