4325b6d403bc02f4bb83a45a5118590149730a9f
DrArtfulBird <56632916+TheBirdEffect@users.noreply.github.com>
2023-05-02 16:18:55 +0200

    finalized tests and scheduler

---
+++ API/Controllers/ScheduleController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entity;
+using API.Util;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace API.Controllers
+{
+    public class ScheduleController : BaseController
+    {
+        private readonly DataContext _context;
+        public ScheduleController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VehicleSchedule>>> SetSchedule()
+        {
+            var scheduler = new Scheduler();
+            var currentOrders = await this._context.Orders.ToListAsync();
+
+            var baseModels = scheduler.calculateSlackAndMapToSchedulingModel(currentOrders);
+
+            foreach (var model in baseModels)
+            {
+                var totalVehicles = await _context.GroundVehicles
+                    .Where(gV => gV.VehicleTypeId == model.Order.VehicleTypeId
+                    ).ToListAsync();
+
+                    
+
+                Console.WriteLine($"Length: {totalVehicles.Count()}; TypeId: {model.Order.VehicleTypeId}");
+
+            //     foreach (var vehicle in availableVehicles)
+            //     {
+            //         if (vehicle.GroundVehicleId.Equals(null))
+            //         {
+            //             var vehicleSchedule = scheduler.assignModelToGroundVehicle(
+            //                 model, vehicle.GroundVehicle
+            //             );
+
+            //             this._context.Add(vehicleSchedule);
+            //         }
+            //         else
+            //         {
+            //             var schedulesOrdered = await this._context.VehicleSchedules
+            //                 .Where(s =>
+            //                 s.GroundVehicle.VehicleTypeId == model.Order.VehicleTypeId)
+            //                 .ToListAsync();
+
+            //             var availableGV = scheduler.returnAvailableGroundVehicles(model, schedulesOrdered);
+            //         }
+            //     }
+            }
+
+            return Ok();
+        }
+    }
+}
