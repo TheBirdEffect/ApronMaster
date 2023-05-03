47089b9c615cec5f83b7f14c954f3d9cb3ddb69f
TheBirdEffect <christoph.frischmuth@web.de>
2023-05-03 14:33:22 +0200

    VehicleScheduling query finalized Ready for #25

--- API/Controllers/VehicleScheduleController.cs
+++ API/Controllers/VehicleScheduleController.cs
@@ -28,5 +28,67 @@ namespace API.Controllers
         {
             return await _context.VehicleSchedules.FindAsync(id);
         }
+
+        [HttpGet("Extended")]
+        public async Task<ActionResult<IEnumerable<VehicleSchedule>>> GetExtendedSchedule()
+        {
+            var query = await (
+                from vS in _context.VehicleSchedules
+                join gV in _context.GroundVehicles
+                on vS.GroundVehicleId equals gV.GroundVehicleId into __groundVehicleIds
+                from vehicle in __groundVehicleIds.DefaultIfEmpty()
+
+                join o in _context.Orders
+                on vS.OrderId equals o.OrderId into __orders
+                from order in __orders.DefaultIfEmpty()
+
+                join f in _context.Flights
+                on vS.Order.FlightId equals f.FlightId into __flights
+                from flight in __flights.DefaultIfEmpty()
+
+                select new VehicleSchedule
+                {
+                    ScheduleId = vS.ScheduleId,
+                    GroundVehicleId = vehicle.GroundVehicleId,
+                    GroundVehicle = new GroundVehicle
+                    {
+                        GroundVehicleId = vehicle.GroundVehicleId,
+                        Name = vehicle.Name,
+                        PositionId = vehicle.PositionId,
+                        Position = vehicle.Position,
+                        VehicleTypeId = vehicle.VehicleTypeId,
+                        VehicleType = vehicle.VehicleType
+                    },
+                    Order = new Order 
+                    {
+                        OrderId = order.OrderId,
+                        FlightId = flight.FlightId,
+                        Flight = new Flight
+                        {
+                            FlightId = flight.FlightId,
+                            FlightNumber = flight.FlightNumber,
+                            Arrival = flight.Arrival,
+                            Departure = flight.Departure,
+                            AircraftTypeId = flight.AircraftTypeId,
+                            Destination = flight.Destination
+                        },
+                        StartOfService = order.StartOfService,
+                        EndOfService = order.EndOfService,
+                        fuelType = order.fuelType,
+                        QtyFuel = order.QtyFuel,
+                        PositionId = order.PositionId,
+                        Position = new Position
+                        {
+                            PositionId = order.Position.PositionId,
+                            Name = order.Position.Name,
+                            IsGate = order.Position.IsGate
+                        }
+                    }
+
+                }
+            ).ToListAsync();
+
+            return Ok(query);
+        }
     }
 }
